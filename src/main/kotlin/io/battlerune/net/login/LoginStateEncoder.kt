43e14ce0f291e001a8479ea80848261b4544ba53
Chad <freesunfury@gmail.com>
2017-10-03 05:09:28 -0500

    More changes

--- src/main/kotlin/io/battlerune/net/login/LoginStateEncoder.kt
+++ src/main/kotlin/io/battlerune/net/login/LoginStateEncoder.kt
@@ -8,19 +8,22 @@ class LoginStateEncoder : MessageToByteEncoder<LoginResponse>() {
 
     override fun encode(ctx: ChannelHandlerContext, msg: LoginResponse, out: ByteBuf) {
 
-        println("reached login state encoder")
-
         // successful
         out.writeByte(2)
 
+        out.writeBoolean(false)
+
         out.writeByte(0)
         out.writeByte(0)
         out.writeByte(0)
         out.writeByte(0)
-        out.writeByte(0)
+
         out.writeByte(2) // rights
-        out.writeByte(0)
-        out.writeShort(1) // player index
+
+        out.writeBoolean(false)
+
+        out.writeShort(1) // index
+
         out.writeByte(1)
 
         ctx.pipeline().remove(this)
