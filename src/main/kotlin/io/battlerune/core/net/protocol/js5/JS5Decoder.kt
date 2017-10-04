b5fdeacdd40f8f1a187c94b7034edc6675571a29
Chad <freesunfury@gmail.com>
2017-10-04 02:04:26 -0500

    More documentation for JS5

--- src/main/kotlin/io/battlerune/core/net/protocol/js5/JS5Decoder.kt
+++ src/main/kotlin/io/battlerune/core/net/protocol/js5/JS5Decoder.kt
@@ -9,14 +9,15 @@ class JS5Decoder : ByteToMessageDecoder() {
     companion object {
         val NORMAL_FILE_REQUEST = 0
         val PRIORITY_FILE_REQUEST = 1
+        val CLIENT_LOGGED_IN = 2
+        val CLIENT_LOGGED_OUT = 3
+        val NEW_ENCRYPTION_USED = 4
     }
 
     override fun decode(ctx: ChannelHandlerContext, incoming: ByteBuf, outgoing: MutableList<Any>) {
 
         if (incoming.readableBytes() >= 4) {
             val type = incoming.readUnsignedByte().toInt()
-
-            println("type $type")
 
             when(type) {
 
@@ -26,12 +27,10 @@ class JS5Decoder : ByteToMessageDecoder() {
                     val file = incoming.readUnsignedShort()
                     val priority = type == 1
 
-                    println("index $index file $file priority $priority")
-
                     outgoing.add(JS5FileRequest(index, file, priority))
                 }
 
-                2, 3, 4 -> {
+                CLIENT_LOGGED_IN, CLIENT_LOGGED_OUT, NEW_ENCRYPTION_USED -> {
                     incoming.skipBytes(3)
                 }
 
