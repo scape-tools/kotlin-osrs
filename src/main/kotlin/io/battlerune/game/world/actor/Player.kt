3a842a2310d136de42c5ffd74c131bbdc84439ce
Chad <freesunfury@gmail.com>
2017-10-05 02:20:15 -0500

    Implemented writePacket function in player

--- src/main/kotlin/io/battlerune/game/world/actor/Player.kt
+++ src/main/kotlin/io/battlerune/game/world/actor/Player.kt
@@ -1,10 +1,16 @@
 package io.battlerune.game.world.actor
 
 import io.battlerune.net.channel.PlayerChannel
+import io.battlerune.net.packet.PacketWriter
 
 class Player(val playerChannel: PlayerChannel) : Pawn() {
 
     lateinit var username: String
     lateinit var password: String
+
+    fun writePacket(writer: PacketWriter) : Player {
+        playerChannel.writeAndFlush(writer)
+        return this
+    }
 
 }
