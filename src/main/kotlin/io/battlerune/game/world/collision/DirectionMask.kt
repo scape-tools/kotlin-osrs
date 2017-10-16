dea706a1229aa0f6bb6c91dc055c07de64f85519
Chad <freesunfury@gmail.com>
2017-10-16 03:06:20 -0500

    Added known directional masks

---
+++ src/main/kotlin/io/battlerune/game/world/collision/DirectionMask.kt
@@ -0,0 +1,10 @@
+package io.battlerune.game.world.collision
+
+object DirectionMask {
+
+    val BLOCK_FLAG_NORTH = 0x1
+    val BLOCK_FLAG_EAST = 0x2
+    val BLOCK_FLAG_SOUTH = 0x4
+    val BLOCK_FLAG_WEST = 0x8
+
+}
